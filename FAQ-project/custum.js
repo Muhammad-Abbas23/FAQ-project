c63d2d753cb150a4bf78701b97e36f0758f6a6b4
Muhammad-Abbas23 <127502250+Muhammad-Abbas23@users.noreply.github.com>
2023-03-13 18:45:36 +0500

    Add files

---
+++ FAQ-project/custum.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+
+    // $.fn.myfunction = function () {
+
+    //     alert('Your pc was not supported this jqurey.');
+    // }
+
+    // $(".btn").click(function () {
+    //     $.fn.myfunction();
+    // });
+
+
+    // $(".btn-1").click(function () {
+
+    //     $("p").fadeOut();
+
+    // });
+
+    // $(".btn-2").click(function () {
+
+    //     $("p").fadeIn();
+
+    // });
+
+    // $(".hide").click(function () {
+
+    //     $('H-P').hide();
+    // });
+
+
+$('.hero-main-bg').hide(0,function(){
+})
+
+$('.hero-main-bg').slideDown(1000,function(){
+    })
+
+    $('.col-6').hide(0,function(){
+    })
+    
+
+    $('.col-6').slideDown(2500,function(){
+
+    })
+
+$(".q-1").click(function(){
+
+$(".a-1").slideToggle();
+
+});
+
+$(".q-2").click(function(){
+
+    $(".a-2").slideToggle();
+    
+    });
+
+    $(".q-3").click(function(){
+
+        $(".a-3").slideToggle();
+        
+        });
+
+        $(".q-4").click(function(){
+
+            $(".a-4").slideToggle();
+            
+            });
+
+
+});
